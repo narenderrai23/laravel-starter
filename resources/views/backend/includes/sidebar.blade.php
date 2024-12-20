b108a1e2ed4edc3e357102cb8b8e768a6f57072f
Narender Rai <narenderrai687@gmail.com>
2024-12-20 13:35:21 +0530

    sadasd

--- resources/views/backend/includes/sidebar.blade.php
+++ resources/views/backend/includes/sidebar.blade.php
@@ -34,6 +34,15 @@ $notifications_latest = optional($notifications)->take(5);
                     'url' => route('backend.notifications.index'),
                 ],
                 [
+                    'group' => 'blogs',
+                    'icon' => 'fa-regular fa-file-lines',
+                    'items' => [
+                        ['name' => 'posts', 'text' => __('Blogs'), 'permission' => 'view_posts'],
+                        ['name' => 'categories', 'text' => __('Categories'), 'permission' => 'view_categories'],
+                        ['name' => 'tags', 'text' => __('Tags'), 'permission' => 'view_tags'],
+                    ],
+                ],
+                [
                     'name' => 'settings',
                     'text' => __('Settings'),
                     'icon' => 'fa-solid fa-gears',
@@ -57,50 +66,30 @@ $notifications_latest = optional($notifications)->take(5);
                     'icon' => 'fa-solid fa-user-shield',
                     'permission' => 'view_roles',
                 ],
+
+                [
+                    'group' => 'logs',
+                    'icon' => 'fa-solid fa-list-ul',
+                    'items' => [
+                        ['route' => 'log-viewer::dashboard', 'text' => 'Log Dashboard', 'permission' => 'view_logs'],
+                        ['route' => 'log-viewer::logs.list', 'text' => 'Daily Log', 'permission' => 'view_logs'],
+                    ],
+                ],
             ];
         @endphp
 
         @foreach ($modules as $module)
-            @isset($module['permission'])
-                <x-backend.sidebar-nav-item :permission="$module['permission']" :url="route('backend.' . $module['name'] . '.index')" :icon="$module['icon']" :text="$module['text']" />
-            @else
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ $module['url'] }}">
-                        <i class="nav-icon {{ $module['icon'] }}"></i>
-                        &nbsp; {{ $module['text'] }}
-                    </a>
-                </li>
-            @endisset
-        @endforeach
-
-        @php
-            $modules = [
-                'blogs' => [
-                    ['name' => 'posts', 'text' => __('Blogs'), 'permission' => 'view_posts'],
-                    ['name' => 'categories', 'text' => __('Categories'), 'permission' => 'view_categories'],
-                    ['name' => 'tags', 'text' => __('Tags'), 'permission' => 'view_tags'],
-                ],
-                'logs' => [
-                    ['route' => 'log-viewer::dashboard', 'text' => 'Log Dashboard', 'permission' => 'view_logs'],
-                    ['route' => 'log-viewer::logs.list', 'text' => 'Daily Log', 'permission' => 'view_logs'],
-                ],
-            ];
-        @endphp
-
-        @foreach (['blogs', 'logs'] as $key)
-            @if ($key === 'blogs' || auth()->user()->can('view_logs'))
+            @if (isset($module['group']))
                 <li class="nav-group" aria-expanded="true">
                     <a class="nav-link nav-group-toggle" href="#">
-                        <i
-                            class="nav-icon {{ $key === 'blogs' ? 'fa-regular fa-file-lines' : 'fa-solid fa-list-ul' }}"></i>
-                        &nbsp; @lang(ucfirst($key))
+                        <i class="nav-icon {{ $module['icon'] }}"></i>&nbsp; @lang(ucfirst($module['group']))
                     </a>
-                    <ul class="nav-group-items compact" style="height: auto">
-                        @foreach ($modules[$key] as $item)
+                    <ul class="nav-group-items compact">
+                        @foreach ($module['items'] as $item)
                             @can($item['permission'])
                                 <li class="nav-item">
                                     <a class="nav-link"
-                                        href="{{ $key === 'blogs' ? route('backend.' . $item['name'] . '.index') : route($item['route']) }}">
+                                        href="{{ $module['group'] === 'blogs' ? route('backend.' . $item['name'] . '.index') : route($item['route']) }}">
                                         <span class="nav-icon"><span class="nav-icon-bullet"></span></span>
                                         &nbsp; {{ $item['text'] }}
                                     </a>
@@ -109,10 +98,12 @@ $notifications_latest = optional($notifications)->take(5);
                         @endforeach
                     </ul>
                 </li>
+            @else
+                <x-backend.sidebar-nav-item :permission="$module['permission'] ?? null" :url="$module['url'] ?? route('backend.' . $module['name'] . '.index')" :icon="$module['icon']" :text="$module['text']" />
             @endif
         @endforeach
-
     </ul>
+
 
     <div class="sidebar-footer border-top d-none d-md-flex">
         <button class="sidebar-toggler" data-coreui-toggle="unfoldable" type="button"></button>
